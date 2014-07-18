acaec470e49483ec7c043a31c4136091f200bc1e
petcomputacaoufrgs <pet@inf.ufrgs.br>
2014-07-18 20:05:09 -0300

    inicializando alguns dados e pondo os devidos getters e setters

--- hidra/instruction.cpp
+++ hidra/instruction.cpp
@@ -3,3 +3,33 @@
 Instruction::Instruction()
 {
 }
+QString Instruction::getMnemonic() const
+{
+    return mnemonic;
+}
+
+void Instruction::setMnemonic(const QString &value)
+{
+    mnemonic = value;
+}
+int Instruction::getValue() const
+{
+    return value;
+}
+
+void Instruction::setValue(int value)
+{
+    value = value;
+}
+int Instruction::getNumberOfArguments() const
+{
+    return numberOfArguments;
+}
+
+void Instruction::setNumberOfArguments(int value)
+{
+    numberOfArguments = value;
+}
+
+
+
