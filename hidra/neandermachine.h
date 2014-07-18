e3a2d3b7f58f090db35c94483a41813eef18b456
petcomputacaoufrgs <pet@inf.ufrgs.br>
2014-07-18 20:49:28 -0300

    inicializando memoria e os registradores no neaner

--- hidra/neandermachine.h
+++ hidra/neandermachine.h
@@ -2,11 +2,14 @@
 #define NEANDERMACHINE_H
 
 #include "machine.h"
+#include <iostream>
 
 class NeanderMachine : public Machine
 {
 public:
     NeanderMachine();
+
+    void printStatusDebug();
 
 private:
     Register* acumulator;
