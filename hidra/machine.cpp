0f04a50f9b8998b980a542106e532b2b0787a30f
Pedro Costa <pedronerung@gmail.com>
2014-07-18 21:57:16 -0300

    criando função para pegar instrução a partir de seu valor

--- hidra/machine.cpp
+++ hidra/machine.cpp
@@ -4,3 +4,14 @@ Machine::Machine(QObject *parent) :
     QObject(parent)
 {
 }
+
+
+const Instruction* Machine::getInstructionFromValue(int desiredInstruction) {
+    QVector<Instruction*>::iterator i;
+    for( i = instructions.begin(); i != instructions.end(); i++) {
+        if((*i)->getValue() == (desiredInstruction & 0xF0)) {
+            return (*i);
+        }
+    }
+    return NULL;
+}
--- hidra/neandermachine.cpp
+++ hidra/neandermachine.cpp
@@ -8,22 +8,52 @@ NeanderMachine::NeanderMachine()
         *i = new Register();
     }
     PC = registers[1];
+    AC = registers[0];
 
     memory = QVector<Byte*>(256);
     QVector<Byte*>::iterator j;
-    for(j = memory.begin(); j != memory.end(); j++) {
-        *j = new Byte();
+    int k;
+    for(k = 0, j = memory.begin(); j != memory.end();k++, j++) {
+        *j = new Byte(k);
     }
+
+    instructions = QVector<Instruction*>(11);
+    instructions[0]  = new Instruction("nop",   0, 0);
+    instructions[1]  = new Instruction("sta",  16, 1);
+    instructions[2]  = new Instruction("lda",  32, 1);
+    instructions[3]  = new Instruction("add",  48, 1);
+    instructions[4]  = new Instruction( "or",  64, 1);
+    instructions[5]  = new Instruction("and",  80, 1);
+    instructions[6]  = new Instruction("not",  96, 0);
+    instructions[7]  = new Instruction("jmp", 128, 1);
+    instructions[8]  = new Instruction( "jn", 144, 1);
+    instructions[9]  = new Instruction( "jz", 160, 1);
+    instructions[10] = new Instruction("hlt", 240, 0);
+
 }
 
 void NeanderMachine::printStatusDebug()
 {
     std::cout << "PC: " << PC->getValue() << std::endl;
+    std::cout << "AC: " << AC->getValue() << std::endl;
     int j;
     QVector<Byte*>::iterator i;
     for(j = 0, i = memory.begin(); i != memory.end(); i++, j=j+1) {
-        (*i)->setValue(j);
-        std::cout << j << ": " << (int)(*i)->getValue() << std::endl;
+        const Instruction *actual = getInstructionFromValue((int)(*i)->getValue());
+        if(actual != NULL) {
+            std::cout << j << ": " << actual->getMnemonic().toStdString() << std::endl;
+            for(int k = 0; k < actual->getNumberOfArguments(); k++) {
+                i++;
+                std::cout << j << ": " << (int)(*i)->getValue() << std::endl;
+            }
+        } else {
+            std::cout << j << ": " << (int)(*i)->getValue() << std::endl;
+        }
+    }
+
+    QVector<Instruction*>::iterator i2;
+    for(j = 0, i2 = instructions.begin(); i2 != instructions.end(); i2++, j=j+1) {
+        std::cout << j << ": " << (*i2)->getMnemonic().toStdString() << " " << (*i2)->getValue() <<  std::endl;
     }
 }
 
