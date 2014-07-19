d9203ae2720cad56ca1bbcaf0c58c819458ba6c3
petcomputacaoufrgs <pet@inf.ufrgs.br>
2014-07-18 22:00:15 -0300

    tirando implementação da getInstructionFromValue da classe machine para ser implementada em cada maquina especifica

--- hidra/machine.cpp
+++ hidra/machine.cpp
@@ -4,14 +4,3 @@ Machine::Machine(QObject *parent) :
     QObject(parent)
 {
 }
-
-
-const Instruction* Machine::getInstructionFromValue(int desiredInstruction) {
-    QVector<Instruction*>::iterator i;
-    for( i = instructions.begin(); i != instructions.end(); i++) {
-        if((*i)->getValue() == (desiredInstruction & 0xF0)) {
-            return (*i);
-        }
-    }
-    return NULL;
-}
