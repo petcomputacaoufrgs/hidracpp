dba12a95dc2cc6221aa077ae9d2afba9801473ae
brunoczim <brunoczim@gmail.com>
2021-08-16 09:18:27 -0300

    small API refactors and comments on base conversor

--- src/core/baseconversor.cpp
+++ src/core/baseconversor.cpp
@@ -1,13 +1,13 @@
 #include "baseconversor.h"
 #include "invalidconversorinput.h"
 
-BaseConversor::BaseConversor()
+BaseConversor::BaseConversor():
+    bits(0),
+    sign(false)
 {
-    bits = 0;
-    sign = false;
 }
 
-uint64_t BaseConversor::mapInput(QChar character, uint64_t base)
+uint64_t BaseConversor::mapInput(QChar character, uint64_t base) const
 {
     int codepoint = character.unicode();
     uint64_t digit = base;
@@ -27,18 +27,19 @@ uint64_t BaseConversor::mapInput(QChar character, uint64_t base)
     return digit;
 }
 
-QChar BaseConversor::mapOutput(uint64_t digit)
+QChar BaseConversor::mapOutput(uint64_t digit) const
 {
-    char map[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    if (digit >= sizeof(map)) {
-        throw InvalidConversorInput("Base inválida");
+    if (digit < 10) {
+        return (char) digit + '0';
+    }
+    if (digit < MAX_BASE) {
+        return (char) digit - 10 + 'A';
     }
 
-    return map[digit];
+    throw InvalidConversorInput("Base inválida");
 }
 
-uint64_t BaseConversor::maxValue(uint64_t base, uint64_t *width, uint64_t atLeast)
+uint64_t BaseConversor::maxValue(uint64_t base, uint64_t *width, uint64_t atLeast) const
 {
     uint64_t result = 0;
     uint64_t tmpResult = 0;
@@ -47,47 +48,55 @@ uint64_t BaseConversor::maxValue(uint64_t base, uint64_t *width, uint64_t atLeas
     while (i < *width || result < atLeast) {
         tmpResult = result * base;
         if (tmpResult < result) {
+            // Overflow check for result *= base.
             throw InvalidConversorInput("Entrada é muito grande.");
         }
         result = tmpResult;
         tmpResult = result + (base - 1);
         if (tmpResult < result) {
+            // Overflow check for result + base - 1.
             throw InvalidConversorInput("Entrada é muito grande.");
         }
         result = tmpResult;
         i++;
     }
 
+    // Stores the actual width of the computed number, given that atLeast might
+    // be bigger than base**width.
     *width = i;
     return result;
 }
 
 // Input functions
 
-void BaseConversor::decode(QString const &digitsQ, uint64_t base, uint64_t *width)
+uint64_t BaseConversor::decode(QString const &digitsQ, uint64_t base, uint64_t *width) const
 {
     uint64_t result = 0;
+    uint64_t numberBits = 0;
     validateBase(base);
 
-    bits = 0;
     *width = 0;
 
     for (QChar ch : digitsQ) {
+        // Allow spaces.
         if (ch != ' ') {
-            result = bits * base;
-            if (result < bits) {
+            result = numberBits * base;
+            if (result < numberBits) {
+                // Overflow check for bits *= base.
                 throw InvalidConversorInput("Entrada é muito grande.");
             }
-            bits = result;
-            result = bits + mapInput(ch, base);
-            if (result < bits) {
+            numberBits = result;
+            result = numberBits + mapInput(ch, base);
+            if (result < numberBits) {
+                // Overflow check for bits += digit.
                 throw InvalidConversorInput("Entrada é muito grande.");
             }
-            bits = result;
+            numberBits = result;
             (*width)++;
         }
     }
 
+    // Checks for if the given number width is valid.
     if (*width > MAX_WIDTH) {
         throw InvalidConversorInput(
                     QString("Largura do inteiro não pode ser maior que ")
@@ -98,159 +107,214 @@ void BaseConversor::decode(QString const &digitsQ, uint64_t base, uint64_t *widt
                     QString("Largura do inteiro não pode ser menor que ")
                     + QString::number(MIN_WIDTH));
     }
+
+    return numberBits;
 }
 
-QString BaseConversor::encode(uint64_t inputBits, uint64_t base, uint64_t width, QChar fill)
+QString BaseConversor::encode(uint64_t inputBits, uint64_t base, uint64_t width, QChar fill) const
 {
     QString output;
     uint64_t current = 0;
-
     validateBase(base);
 
+    // Computes each present digit.
     while (current < width || inputBits > 0) {
         output.append(mapOutput(inputBits % base));
         inputBits /= base;
         current++;
     }
 
+    // If not large enough, adds a fill character.
     while (current < width) {
         output.append(fill);
         current++;
     }
 
+    // Most significant digits are computed first and are placed in the
+    // beginning, but they should be at the end, then reverse the string.
     std::reverse(output.begin(), output.end());
     return output;
 }
 
-BaseConversor& BaseConversor::inputPositive(QString digitsQ, uint64_t base)
+// Input functions
+
+BaseConversor& BaseConversor::inputPositive(QString const &digitsQ, uint64_t base)
 {
     uint64_t width;
-    decode(digitsQ, base, &width);
-    sign = false;
+    // Very simple. Just decoding.
+    this->bits = decode(digitsQ, base, &width);
+    // Positive, not negative.
+    this->sign = false;
     return *this;
 }
 
-BaseConversor& BaseConversor::inputSignMagnitude(QString digitsQ, uint64_t base)
+BaseConversor& BaseConversor::inputSignMagnitude(QString const &digitsQ, uint64_t base)
 {
     uint64_t width;
+    // Trimming because spaces don't matter.
     QString digitsTrimmed = digitsQ.trimmed();
 
+    // We need at least the sign.
     if (digitsTrimmed.length() == 0) {
         throw InvalidConversorInput("Sinal magnitude precisa de um dígito para sinal.");
     }
 
+    // Only 0 or 1 is valid for the sign.
     switch (digitsTrimmed[0].unicode()) {
     case '0':
-        sign = false;
+        this->sign = false;
         break;
     case '1':
-        sign = true;
+        this->sign = true;
         break;
     default:
         throw InvalidConversorInput("Dígito inválido para a base");
         break;
     }
 
+    // Removes the sign (position 0, 1 char).
     digitsTrimmed.remove(0, 1);
-    decode(digitsTrimmed, base, &width);
+    // Now we can simply decode the remaining digits.
+    this->bits = decode(digitsTrimmed, base, &width);
     return *this;
 }
 
-BaseConversor& BaseConversor::inputOnesComplement(QString digitsQ, uint64_t base)
+BaseConversor& BaseConversor::inputOnesComplement(QString const &digitsQ, uint64_t base)
 {
     uint64_t width = 0;
     uint64_t maxValue = 0;
-    decode(digitsQ, base, &width);
+    uint64_t numberBits = decode(digitsQ, base, &width);
 
+    // Computes B**width - 1 (the complement).
     maxValue = this->maxValue(base, &width, 0);
 
-    sign = bits >= (maxValue / 2 + maxValue % 2);
-    if (sign) {
-        bits = maxValue - bits;
+    // The actual formula is (bits >= (maxValue + 1) / 2).
+    //
+    // floor(B**width / 2) - 1 is positive
+    //
+    // floor(B**width / 2) is negative
+    this->sign = numberBits >= (maxValue / 2 + maxValue % 2);
+    if (this->sign) {
+        numberBits = maxValue - numberBits;
     }
 
+    this->bits = numberBits;
     return *this;
 }
 
-BaseConversor& BaseConversor::inputTwosComplement(QString digitsQ, uint64_t base)
+BaseConversor& BaseConversor::inputTwosComplement(QString const &digitsQ, uint64_t base)
 {
     uint64_t width = 0;
     uint64_t maxValue = 0;
-    decode(digitsQ, base, &width);
+    uint64_t numberBits = decode(digitsQ, base, &width);
 
+    // Computes B**width - 1  (the complement - 1).
     maxValue = this->maxValue(base, &width, 0);
 
-    sign = bits > maxValue / 2;
-    if (sign) {
-        bits = maxValue - bits + 1;
+    // floor(B**width / 2) is positive
+    //
+    // floor(B**width / 2) + 1 is negative
+    this->sign = numberBits > maxValue / 2;
+    if (this->sign) {
+        numberBits = maxValue - numberBits + 1;
     }
+
+    this->bits = numberBits;
     return *this;
 }
 
 // Output functions
 
-QString BaseConversor::outputPositive(uint64_t base, uint64_t width)
+QString BaseConversor::outputPositive(uint64_t base, uint64_t width) const
 {
-    if (sign) {
+    // Very simple. Just checks for negative.
+    if (this->sign) {
         throw InvalidConversorInput("Inteiro positivo não admite valores negativos.");
     }
 
-    return this->encode(bits, base, width, '0');
+    // Then simply encodes the given number with the regular to-string algorithm.
+    return this->encode(this->bits, base, width, '0');
 }
 
-QString BaseConversor::outputSignMagnitude(uint64_t base, uint64_t width)
+QString BaseConversor::outputSignMagnitude(uint64_t base, uint64_t width) const
 {
-    QString output = this->encode(bits, base, width, '0');
-    output.prepend(sign ? '1' : '0');
+    // Simply encodes the given number with the regular to-string algorithm.
+    QString output = this->encode(this->bits, base, width, '0');
+    // Then prepend the sign, which does not count for the width.
+    output.prepend(this->sign ? '1' : '0');
     return output;
 }
 
-QString BaseConversor::outputOnesComplement(uint64_t base, uint64_t width)
+QString BaseConversor::outputOnesComplement(uint64_t base, uint64_t width) const
 {
     uint64_t outputWidth = width;
-    uint64_t outputBits = bits;
-    uint64_t maxValue, atLeast;
+    uint64_t outputBits = this->bits;
+    // Dummy value.
+    uint64_t maxValue = 0;
+    uint64_t atLeast = 0;
+    // Only if positive sign.
     QChar fill = '0';
 
-    if (sign) {
+    if (this->sign) {
+        // Computes B**width - 1 (the complement). Width must be at least
+        // enough to store the complement.
         maxValue = this->maxValue(base, &outputWidth, outputBits);
+        // Computes the complement of the stored bits.
         outputBits = maxValue - outputBits;
+        // Fill is the highest digit if negative.
         fill = mapOutput(base - 1);
     } else {
+        // Width must be at least enough to store the complement.
         atLeast = outputBits * 2;
         if (atLeast < outputBits) {
+            // A positive number might be too large for B-1's complement.
+            // Check overflow.
             throw InvalidConversorInput("Entrada é muito grande.");
         }
+        // Computes the width.
         this->maxValue(base, &outputWidth, outputBits * 2);
     }
 
+    // If no error was thrown, just convert the complemented number to string.
     return this->encode(outputBits, base, outputWidth, fill);
 }
 
 
-QString BaseConversor::outputTwosComplement(uint64_t base, uint64_t width)
+QString BaseConversor::outputTwosComplement(uint64_t base, uint64_t width) const
 {
     uint64_t outputWidth = width;
-    uint64_t outputBits = bits;
-    uint64_t maxValue, atLeast;
+    uint64_t outputBits = this->bits;
+    // Dummy value.
+    uint64_t maxValue = 0;
+    uint64_t atLeast = 0;
+    // Only if positive sign.
     QChar fill = '0';
 
-    if (sign) {
+    if (this->sign) {
+        // Computes B**width - 1 (the complement - 1). Width must be at least
+        // enough to store the complement.
         maxValue = this->maxValue(base, &outputWidth, outputBits);
+        // Computes the complement of the stored bits.
         outputBits = maxValue - outputBits + 1;
+        // Fill is the highest digit if negative.
         fill = mapOutput(base - 1);
     } else {
+        // Width must be at least enough to store the complement.
         atLeast = outputBits * 2;
         if (atLeast < outputBits) {
+            // A positive number might be too large for B's complement.
+            // Check overflow.
             throw InvalidConversorInput("Entrada é muito grande.");
         }
+        // Computes the width.
         this->maxValue(base, &outputWidth, outputBits * 2);
     }
 
+    // If no error was thrown, just convert the complemented number to string.
     return this->encode(outputBits, base, outputWidth, fill);
 }
 
-void BaseConversor::validateBase(uint64_t base)
+void BaseConversor::validateBase(uint64_t base) const
 {
     if (base < MIN_BASE) {
         throw InvalidConversorInput(QString("Base precisa ser no mínimo ") + QString::number(MIN_BASE));
--- src/core/baseconversor.h
+++ src/core/baseconversor.h
@@ -11,37 +11,97 @@ class BaseConversor
 public:
     BaseConversor();
 
-    // Input functions
-    BaseConversor& inputPositive(QString digits, uint64_t base);
-    BaseConversor& inputSignMagnitude(QString digits, uint64_t base);
-    BaseConversor& inputOnesComplement(QString digits, uint64_t base);
-    BaseConversor& inputTwosComplement(QString digits, uint64_t base);
+    /// Inputs a positive (unsigned) number into the conversor in a textual
+    /// representation. Max value allowed is 2**64 - 1.
+    ///
+    /// Throws an error if there are too many digits or invalid digits.
+    BaseConversor& inputPositive(QString const &digits, uint64_t base);
+    /// Inputs a sign magnitude number into the conversor in a textual
+    /// representation. Max value allowed is +2**64 - 1 and minimum 2**64 - 1.
+    ///
+    /// Throws an error if there are too many digits, invalid digits, or sign
+    /// is not 0 or 1.
+    BaseConversor& inputSignMagnitude(QString const &digits, uint64_t base);
+    /// Inputs a B-1's complement number into the conversor in a textual
+    /// representation. Max value allowed is +2**63 - 1 and minimum 2**63 - 1.
+    ///
+    /// Throws an error if there are too many digits or invalid digits.
+    BaseConversor& inputOnesComplement(QString const &digits, uint64_t base);
+    /// Inputs a B's complement number into the conversor in a textual
+    /// representation. Max value allowed is +2**63 - 1 and minimum 2**63.
+    ///
+    /// Throws an error if there are too many digits or invalid digits.
+    BaseConversor& inputTwosComplement(QString const &digits, uint64_t base);
 
-    // Output functions
-    QString outputPositive(uint64_t base, uint64_t width);
-    QString outputSignMagnitude(uint64_t base, uint64_t width);
-    QString outputOnesComplement(uint64_t base, uint64_t width);
-    QString outputTwosComplement(uint64_t base, uint64_t width);
+    /// Outputs the stored number in positive (unsigned) format as text, with
+    /// at least the given width of digits.
+    ///
+    /// Throws an error if the stored number is negative, or the given width
+    /// is invalid.
+    QString outputPositive(uint64_t base, uint64_t width) const;
+    /// Outputs the stored number in sign magnitude format as text, with at
+    /// least the given width of digits.
+    ///
+    /// Throws an error if the the given width is invalid.
+    QString outputSignMagnitude(uint64_t base, uint64_t width) const;
+    /// Outputs the stored number in B-1's complement format as text, with at
+    /// least the given width of digits.
+    ///
+    /// Throws an error if the the given width is invalid.
+    QString outputOnesComplement(uint64_t base, uint64_t width) const;
+    /// Outputs the stored number in B's complement format as text, with at
+    /// least the given width of digits.
+    ///
+    /// Throws an error if the the given width is invalid.
+    QString outputTwosComplement(uint64_t base, uint64_t width) const;
 
-private:  
+private:
+    /// Base 2 representation of the conversible numbers.
+    /// Holds at most 64 bits.
     uint64_t bits;
+    /// Sign of the stored number. More specifically, it means whether the sign
+    /// is negative. Always `false` for positive number format.
     bool sign;
 
-    uint64_t mapInput(QChar character, uint64_t base);
-    QChar mapOutput(uint64_t digit);
+    /// Maps a character to a digit in a given base.
+    ///
+    /// Throws an error if digit is invalid for the base.
+    uint64_t mapInput(QChar character, uint64_t base) const;
+    /// Maps a digit to a character in a given base.
+    ///
+    /// Throws an error if digit is too big for any valid base.
+    QChar mapOutput(uint64_t digit) const;
 
-    uint64_t maxValue(uint64_t base, uint64_t *width, uint64_t atLeast);
+    /// Gets the maximum value for a number in the given base with the given
+    /// width in digits, and greater than or equals to the given `atLeast`
+    /// number, even if it surpasses the expected width. The actual width is
+    /// placed in the width in-out parameter.
+    ///
+    /// Throws an error on overflow.
+    uint64_t maxValue(uint64_t base, uint64_t *width, uint64_t atLeast) const;
 
-    void decode(QString const &digitsQ, uint64_t base, uint64_t *width);
-    QString encode(uint64_t bits, uint64_t base, uint64_t width, QChar fill);
+    /// Decodes a given string of digits in the given base into an integer.
+    /// Places in the `width` out parameter the count of digits.
+    ///
+    /// Throws an error if an invalid digit is found or the number is too big.
+    uint64_t decode(QString const &digitsQ, uint64_t base, uint64_t *width) const;
+    /// Encodes into a string of digits in the given base from an integer.
+    /// The total number of digits will be at least `width`, filling with the
+    /// character `fill` if necessary.
+    QString encode(uint64_t bits, uint64_t base, uint64_t width, QChar fill) const;
 
-    void validateBase(uint64_t base);
+    /// Throws an exception if the given base is invalid.
+    void validateBase(uint64_t base) const;
 
+    /// Maximum width in digits.
     static const int MAX_WIDTH = 64;
+    /// Minimum width in digits.
     static const int MIN_WIDTH = 1;
 
-    static const int MIN_BASE = 2;
+    /// Maximum base.
     static const int MAX_BASE = 36;
+    /// Minimum base.
+    static const int MIN_BASE = 2;
 };
 
 #endif // BASECONVERSOR_H
