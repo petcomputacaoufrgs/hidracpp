ca5c519f465e2a368b11fdcc1398bf22ebf43eb7
JHLM <josehenriquelm@gmail.com>
2022-10-12 14:36:33 -0300

    CESAR Fetch & Decode
    Co-authored-by: Galeano Folle Domingues <TakatJJ@users.noreply.github.com>
    Co-authored-by: jvhendrix <jvhendrix@users.noreply.github.com>

--- src/core/instruction.cpp
+++ src/core/instruction.cpp
@@ -5,7 +5,7 @@ Instruction::Instruction()
 {
 }
 
-Instruction::Instruction(int numBytes, QString bitPattern, InstructionCode instructionCode, QString assemblyFormat, InstructionGroup instructionGroup)
+Instruction::Instruction(int numBytes, QString bitPattern, InstructionCode instructionCode, QString assemblyFormat, InstructionGroup instructionGroup = NO_GROUP)
 {
     this->numBytes = numBytes; // 0 if variable
     this->bitPattern = bitPattern;
