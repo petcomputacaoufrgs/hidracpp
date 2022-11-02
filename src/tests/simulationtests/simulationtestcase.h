1106e4a84f012acaf41d78253a1155a1b7dac35f
JHLM <josehenriquelm@gmail.com>
2022-11-02 17:59:00 -0300

    Cleanup before merge

--- src/tests/simulationtests/simulationtestcase.h
+++ src/tests/simulationtests/simulationtestcase.h
@@ -1,3 +1,1 @@
-#define PUT_INSTRUCTION_IN_MEMORY(machine, inst, param)\
-machine.setMemoryValue(0, inst);\
-machine.setMemoryValue(1, param); 
+#define PUT_INSTRUCTION_IN_MEMORY(machine, inst, param) machine.setMemoryValue(0, inst); machine.setMemoryValue(1, param); 
