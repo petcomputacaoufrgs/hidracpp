71eba44849ad01d32ec035c2cd36e63e2eeda4d9
JHLM <josehenriquelm@gmail.com>
2022-11-02 22:05:40 -0300

    Added some test data to MOV

--- src/tests/simulationtests/Cesar/tst_cesar.cpp
+++ src/tests/simulationtests/Cesar/tst_cesar.cpp
@@ -22,6 +22,7 @@ private slots:
     // GROUP_RETURN_SUBROUTINE
     // GROUP_ONE_OPERAND
     // GROUP_TWO_OPERAND
+    void test_MOV_data();
     void test_MOV();
 
 
@@ -45,13 +46,62 @@ void CesarMachineTest::init()
     testedMachine.setRunning(true);
 }
 
+void CesarMachineTest::test_MOV_data()
+{
+    QTest::addColumn<int>("src");
+    QTest::addColumn<int>("dst");
+    QTest::addColumn<int>("src_value");
+    QTest::addColumn<int>("dst_value");
+    QTest::addColumn<int>("expected_src");
+    QTest::addColumn<int>("expected_dst");
+    QTest::addColumn<int>("checked_address");
+    QTest::addColumn<int>("expected_address_value");
+    QTest::addColumn<int>("addressing_mode_1");
+    QTest::addColumn<int>("addressing_mode_2");
+                                        // src  dst    src_v    dst_v   exp_src exp_dst ch_add  exp_add am1     am2
+    QTest::newRow("Register-Register")   << 0   << 1    << 5    << 0    << 5    << 5    << -1   << -1   << 0    << 0;
+    //MOV R0 (R1)+ (R0 = 5, R1 = 0) ->
+    //MEM(R1) = REG(R0); R1+=2;
+    QTest::newRow("Register-Post-Inc")   << 0   << 1    << 5    << 14   << 5    << 16   << 15   << 5    << 0    << 1;
+    //MOV R0 -(R1) (R0 = 5, R1 = 0) -> 
+    //R1-=2; MEM(R1) = REG(R0);
+    QTest::newRow("Register-Pre-Dec")    << 0   << 1    << 5    << 16   << 5    << 14   << 15   << 5    << 0    << 2;
+
+}
+
 void CesarMachineTest::test_MOV()
 {
-    testedMachine.setRegisterValue("R0", 5);
-    testedMachine.setMemoryValue(0, 0b10010000);
-    testedMachine.setMemoryValue(1, 0b00000001);
+    QFETCH(int, src);
+    QFETCH(int, dst);
+    QFETCH(int, src_value);
+    QFETCH(int, dst_value);
+    QFETCH(int, expected_src);
+    QFETCH(int, expected_dst);
+    QFETCH(int, checked_address);
+    QFETCH(int, expected_address_value);   
+    QFETCH(int, addressing_mode_1);
+    QFETCH(int, addressing_mode_2);
+    
+    testedMachine.setRegisterValue(src, src_value);
+    testedMachine.setRegisterValue(dst, dst_value);
+    int opcode = 0b1001000000000000;
+    opcode |= src << 6;
+    opcode |= addressing_mode_1 << 9;
+    opcode |= dst;
+    opcode |= addressing_mode_2 << 3;
+    testedMachine.memoryWriteTwoByte(0, opcode);
     testedMachine.step();
-    QCOMPARE(testedMachine.getRegisterValue("R1"), 5);
+    
+    QCOMPARE(testedMachine.getRegisterValue(src), expected_src);
+    
+    if(checked_address == -1)
+    {
+        QCOMPARE(testedMachine.getRegisterValue(dst), expected_dst);
+    }
+    else{
+        QCOMPARE(testedMachine.getMemoryValue(checked_address), expected_address_value);
+    }
+    
 
 }
 
