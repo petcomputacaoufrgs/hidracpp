a2c08580ace460ee202499b1e82ad83984515a5b
jvhendrix <joaovitorsouza5000@gmail.com>
2023-02-16 17:16:22 -0300

    visor has same width as main window and is now frameless

--- src/gui/visorwidget.cpp
+++ src/gui/visorwidget.cpp
@@ -6,7 +6,8 @@ VisorWidget::VisorWidget(QWidget *parent) :
     ui(new Ui::VisorWidget)
 {
     ui->setupUi(this);
-    this->setAttribute( Qt::WA_QuitOnClose, false);
+    this->setAttribute(Qt::WA_QuitOnClose, false);
+    this->setWindowFlags(Qt::FramelessWindowHint);
 }
 
 VisorWidget::~VisorWidget()
